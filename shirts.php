042643709110fc9e4af198321db633a7ee5c5b05
Jesse Robinson <j3robinson1@gmail.com>
2015-04-29 23:25:57 -0400

    added nested arrays for shirts

--- shirts.php
+++ shirts.php
@@ -1,4 +1,18 @@
 <?php
+  $products = array();
+  $products[0] = array(
+    "name" => "Logo Shirt, Gray",
+    "price" => 20,
+    "img" => "img/shirts/shirt-106.jpg");
+  $products[1] = array(
+    "name" => "Logo Shirt, Turquoise",
+    "price" => 20,
+    "img" => "img/shirts/shirt-107.jpg");
+  $products[2] = array(
+    "name" => "Logo Shirt, Orange",
+    "price" => 25,
+    "img" => "img/shirts/shirt-108.jpg");
+?><?php
 
   $page_title = "Mike's Shirts";
   include('inc/header.php'); 
@@ -6,9 +20,14 @@
 ?>
 
   <div class="section page">
-
-    <h1>Shirts</h1>
-
+    <div class="wrapper">
+      <h1>Mike&rsquo;s full catalog of shirts</h1>
+      <ul class="products">
+          <?php foreach($products as $product) {?>
+          <a href="#"><img src="<?php echo $products["img"] ?> alt="<?php echo $product["name"]; ?>>
+            <p>view details</p></a>
+          <?php } ?>
+      </ul>
   </div>
   
 <?php include('inc/footer.php'); ?>
